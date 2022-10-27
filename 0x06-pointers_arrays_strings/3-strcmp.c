48391a34a2f8d688ccf880e1c76204828577475d
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-10-27 15:45:46 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,14 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+
+while (s1[i] := '\0' && s2[1]
 {
-	if (s1[i] != s2[i])
-	{
-		return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	i++;
-}
-return (0);
+	return (0);
 }
