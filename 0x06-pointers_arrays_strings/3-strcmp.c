d052b560f4a2db55c655035a5bb135845fd922b9
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-10-26 23:15:20 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,23 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * _strcmp - function that copies a string.
- * 
- *  @s1: s1
- *  @s2: s2
- *  Return: Always 0.
+ * _strcmp - compares two strings values
+ * @s1: string 1
+ * @s2: string 2
+ * Return the difference in s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+	i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+	if (s1[i] != s2[i])
 	{
-		i++;
+		return (s1[i] - s2[i]);
 	}
-	return (s1[i] - s2[i]);
+	i++;
+}
+return (0);
 }
