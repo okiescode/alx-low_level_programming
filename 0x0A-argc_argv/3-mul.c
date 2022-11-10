0b070065601325bdd49015ae6b27f5ccc053a1e0
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-11-09 16:43:43 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - multiplies two numbers followed by newline or
- *nprint Error if prigram does not recieve teo arguments
- * @argc: argument count
- *
- * Return: 0(success) or 1 if not two arguments
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 if error.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argc != 3)
+	int a, b, c;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+		printf("%d\n", c);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
 }
