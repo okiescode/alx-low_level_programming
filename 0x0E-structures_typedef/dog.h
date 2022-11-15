0fa17e095664749e7b00a55799154aef808cf12c
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-11-15 15:36:17 -0800

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Defines a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog with the following elements
+ * name, type = char *::age, type = float::owner, type = char *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t -Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
