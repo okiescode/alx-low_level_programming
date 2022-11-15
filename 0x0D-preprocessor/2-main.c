117b3fe13db6e42d0557bae50095dca132e5e300
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-11-15 14:43:35 -0800

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints the file name it was compiled from
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
