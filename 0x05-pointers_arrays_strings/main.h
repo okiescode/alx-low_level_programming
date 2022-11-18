a30c5501e6bd717913acf3b40c2a52de73564c3e
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-11-17 18:10:00 -0800

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_h_
+#define _MAIN_H_
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+
+#endif
