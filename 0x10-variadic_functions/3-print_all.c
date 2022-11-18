ff5a1da337b9c5bab76d9e141436aa196941a7c6
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-11-17 19:54:24 -0800

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,19 +2,18 @@
 
 /**
  * print_all - prints anything
- * @format: a list of types of arguments passed
+ * @format: a list of types of arguments passed to the function.
  *
- * Return: nothing
+ * Return: no return.
  */
-
 void print_all(const char * const format, ...)
 {
-	va_list list;
+	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
-
-	va_start(list, format);
+	
+	va_start(valist, format);
 	while (format && format[i])
 	{
 		j = 0;
@@ -29,21 +28,21 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(list, int)), c = 1;
+			printf("%c", va_arg(valist, int)), c = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(list, int)), c = 1;
+			printf("%d", va_arg(valist, int)), c = 1;
 			break;
-		case 'f':
-		        printf("%f", va_arg(list, double)), c = 1;
-		        break;
 		case 's':
-		        str = va_arg(list, char *), c = 1;
-		        if (!str)
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
 			{
-			       printf("(nil)");
-		               break;
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
 		} i++;
 	}
-	printf("\n"), va_end(list);
+	printf("\n"), va_end(valist);
 }
