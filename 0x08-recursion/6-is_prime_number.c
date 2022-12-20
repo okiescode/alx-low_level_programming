c5e31d01f01536878519bfcb1ec13d52e6812b86
evbotaokiemute@gmail.com <evbotaokiemute@gmail.com>
2022-12-20 04:57:06 -0800

    0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - determine if a number is a prime number
+ * @n: int number
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int prime_number(int divider, int n);
+
+	int divider = 2;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n % n == 0 || n % 1 == 0)
+	{
+		if (prime_number(divider, n) != 0)
+		{
+			return (0);
+		}
+		else
+		{
+			++divider;
+			return (prime_number(divider, n));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
